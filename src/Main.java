723c6b91acf2f2f99b03038033469d781f8e356f
Vlad Manea <vladmanea@Vlads-MacBook-Air.local>
2025-07-08 21:54:32 +0100

    Add Factorial logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Welcome to the Factorial Calculator!");
+    int n = sc.nextInt();
+    boolean calculate = true;
+    while (calculate) {
+      System.out.println("Enter a non-negative integer");
+      int count = 0;
+      int res = 1;
+      while (count < n) {
+        res = res * (count + 1);
+        count++;
+      }
+      System.out.println("Factorial " + n + "=" + res);
+      System.out.println("Would you like to continue: ");
+      String input = sc.next();
+      if (input.equals("no")) {
+        calculate = false;
+      }
     }
+    sc.close();
+  }
 }
